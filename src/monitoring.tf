86e7f98a06422de84662151cf37a4e5a6e5087b9
chrisghill <chris@massdriver.cloud>
2026-04-18 12:18:24 -0600

    move to public modules for alarms

--- src/monitoring.tf
+++ src/monitoring.tf
@@ -1,82 +1,68 @@
 
 locals {
-  threshold_cpu    = 0.6
-  threshold_disk   = 0.6
-  threshold_memory = 0.8
-
   metrics = {
     "cpu" = {
-      metric   = "cloudsql.googleapis.com/database/cpu/utilization"
-      resource = "cloudsql_database"
+      metric    = "cloudsql.googleapis.com/database/cpu/utilization"
+      resource  = "cloudsql_database"
+      threshold = 0.6
     }
     "disk" = {
-      metric   = "cloudsql.googleapis.com/database/disk/utilization"
-      resource = "cloudsql_database"
+      metric    = "cloudsql.googleapis.com/database/disk/utilization"
+      resource  = "cloudsql_database"
+      threshold = 0.6
     }
     "memory" = {
-      metric   = "cloudsql.googleapis.com/database/memory/utilization"
-      resource = "cloudsql_database"
+      metric    = "cloudsql.googleapis.com/database/memory/utilization"
+      resource  = "cloudsql_database"
+      threshold = 0.8
     }
   }
 }
 
 module "alarm_channel" {
-  source      = "github.com/massdriver-cloud/terraform-modules//gcp-alarm-channel?ref=bfcf556"
+  source      = "massdriver-cloud/gcp-alarm-channel/massdriver"
   md_metadata = var.md_metadata
 }
 
-
 module "database_cpu_alarm" {
-  source                  = "github.com/massdriver-cloud/terraform-modules//gcp-monitoring-utilization-threshold?ref=cafdc89"
+  source                  = "massdriver-cloud/gcp-metric-alarm/massdriver"
   notification_channel_id = module.alarm_channel.id
   md_metadata             = var.md_metadata
   display_name            = "CPU Utilization"
-  message                 = "Cloud SQL Postgres ${google_sql_database_instance.main.self_link}: CPU Utilization over threshold ${local.threshold_cpu * 100}%"
-  alarm_name              = "${google_sql_database_instance.main.self_link}-highCPU"
+  message                 = "Cloud SQL Postgres ${google_sql_database_instance.main.self_link}: CPU Utilization over threshold ${local.metrics["cpu"].threshold * 100}%"
+  alarm_name              = "${google_sql_database_instance.main.name}-highCPU"
   metric_type             = local.metrics["cpu"].metric
   resource_type           = local.metrics["cpu"].resource
-  threshold               = local.threshold_cpu
-  period                  = 60
+  threshold               = local.metrics["cpu"].threshold
+  comparison              = "COMPARISON_GT"
   duration                = 60
-
-  depends_on = [
-    google_sql_database_instance.main
-  ]
 }
 
 
 module "database_disk_alarm" {
-  source                  = "github.com/massdriver-cloud/terraform-modules//gcp-monitoring-utilization-threshold?ref=cafdc89"
+  source                  = "massdriver-cloud/gcp-metric-alarm/massdriver"
   notification_channel_id = module.alarm_channel.id
   md_metadata             = var.md_metadata
   display_name            = "Disk Capacity"
-  message                 = "Cloud SQL Postgres ${google_sql_database_instance.main.self_link}: Disk capacity over threshold ${local.threshold_disk * 100}%"
-  alarm_name              = "${google_sql_database_instance.main.self_link}-highDisk"
+  message                 = "Cloud SQL Postgres ${google_sql_database_instance.main.self_link}: Disk capacity over threshold ${local.metrics["disk"].threshold * 100}%"
+  alarm_name              = "${google_sql_database_instance.main.name}-highDisk"
   metric_type             = local.metrics["disk"].metric
   resource_type           = local.metrics["disk"].resource
-  threshold               = local.threshold_disk
-  period                  = 60
+  threshold               = local.metrics["disk"].threshold
+  comparison              = "COMPARISON_GT"
   duration                = 60
-
-  depends_on = [
-    google_sql_database_instance.main
-  ]
 }
 
 module "database_memory_alarm" {
-  source                  = "github.com/massdriver-cloud/terraform-modules//gcp-monitoring-utilization-threshold?ref=cafdc89"
+  source                  = "massdriver-cloud/gcp-metric-alarm/massdriver"
   notification_channel_id = module.alarm_channel.id
   md_metadata             = var.md_metadata
   display_name            = "Memory Capacity"
-  message                 = "Cloud SQL Postgres ${google_sql_database_instance.main.self_link}: Memory capacity over threshold ${local.threshold_memory * 100}%"
-  alarm_name              = "${google_sql_database_instance.main.self_link}-highMemory"
+  message                 = "Cloud SQL Postgres ${google_sql_database_instance.main.self_link}: Memory capacity over threshold ${local.metrics["memory"].threshold * 100}%"
+  alarm_name              = "${google_sql_database_instance.main.name}-highMemory"
   metric_type             = local.metrics["memory"].metric
   resource_type           = local.metrics["memory"].resource
-  threshold               = local.threshold_memory
-  period                  = 60
+  threshold               = local.metrics["memory"].threshold
+  comparison              = "COMPARISON_GT"
   duration                = 60
-
-  depends_on = [
-    google_sql_database_instance.main
-  ]
 }
